723b07ed4c6773f5d7e2a24a6a7e5478aeeed4b4
josiw <josiwasd@gmail.com>
2025-07-15 22:17:16 -0300

    logo

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,13 +1,11 @@
-import { ShoppingBagIcon } from '@heroicons/react/24/outline';
 import CartModal from 'components/cart/modal';
+import LogoIcon from 'components/icons/logo-icon';
 import { getMenu } from 'lib/shopify';
 import { Menu } from 'lib/shopify/types';
 import Link from 'next/link';
 import { Suspense } from 'react';
 import MobileMenu from './mobile-menu';
 import Search, { SearchSkeleton } from './search';
-
-const { SITE_NAME } = process.env;
 
 export async function Navbar() {
   const menu = await getMenu('next-js-frontend-header-menu');
@@ -26,11 +24,9 @@ export async function Navbar() {
             prefetch={true}
             className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6"
           >
-            {/*<LogoSquare />*/}
-            <ShoppingBagIcon className="h-6" />
-            <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
-              {SITE_NAME}
-            </div>
+            {/* Logo implementado */}
+            <LogoIcon className="h-32 w-32 md:h-32 md:w-32" />
+            {/* Removido el texto del SITE_NAME ya que el logo incluye el nombre */}
           </Link>
           {menu.length ? (
             <ul className="hidden gap-6 text-sm md:flex md:items-center">
--- components/opengraph-image.tsx
+++ components/opengraph-image.tsx
@@ -1,7 +1,7 @@
 import { readFile } from 'fs/promises';
 import { ImageResponse } from 'next/og';
 import { join } from 'path';
-import LogoIcon from './icons/logo';
+import LogoIcon from './icons/logo-icon';
 
 export type Props = {
   title?: string;
@@ -10,12 +10,6 @@ export type Props = {
 export default async function OpengraphImage(
   props?: Props
 ): Promise<ImageResponse> {
-  const { title } = {
-    ...{
-      title: process.env.SITE_NAME
-    },
-    ...props
-  };
 
   const file = await readFile(join(process.cwd(), './fonts/Inter-Bold.ttf'));
   const font = Uint8Array.from(file).buffer;
@@ -26,7 +20,6 @@ export default async function OpengraphImage(
         <div tw="flex flex-none items-center justify-center border border-neutral-700 h-[160px] w-[160px] rounded-3xl">
           <LogoIcon width="64" height="58" fill="white" />
         </div>
-        <p tw="mt-12 text-6xl font-bold text-black">{title}</p>
       </div>
     ),
     {
